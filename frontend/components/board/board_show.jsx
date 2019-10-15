f478c68d3aaaac28a7f9250c28359783be9d74e4
Abby Xu <abby.tsui2016@gmail.com>
2019-10-15 14:31:48 -0700

    switch branch

--- frontend/components/board/board_show.jsx
+++ frontend/components/board/board_show.jsx
@@ -1,35 +1,4 @@
 import React from "react";
-
-class BoardShow extends React.Component {
-
-	componentDidMount() {
-		this.props.fetchBoard();
-	}
-
-	render() {
-
-		const { board, items } = this.props;
-		if (!board) {
-			return null;
-		}
-
-		const pinItems = items.map((item) => {
-			return <div>hasn't built ItemComponent</div>
-			// return <ItemIndividual item={item} /> 
-		})
-
-		return (
-			<div>
-				{ pinItems }
-			</div>
-		)
-	}
-	
-}
-
-export default BoardShow;
-
-import React from "react";
 import BoardShowItem from "./board_show_item";
 
 class BoardShow extends React.Component {
@@ -55,7 +24,6 @@ class BoardShow extends React.Component {
 			</div>
 		)
 	}
-
 }
 
 export default BoardShow;
